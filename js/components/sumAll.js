7ef0747d86e46dd4b058000d7142e9c2fb41f9a9
Rimantas <belauzas@yahoo.com>
2021-01-26 14:55:51 +0200

    Import export

---
+++ js/components/sumAll.js
@@ -0,0 +1,13 @@
+import { arraySum } from './arraySum.js';
+
+function sumAll(data) {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        const numberPair = data[i];
+        const sum = arraySum(numberPair);
+
+        console.log(numberPair, sum);
+    }
+}
+
+export { sumAll }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log('aresgfd');
+import { sumAll } from './components/sumAll.js';
+import { duomenys1 } from './data/duomenys1.js';
+import { duomenys2 } from './data/duomenys2.js';
+
+sumAll(duomenys1);
+sumAll(duomenys2);
